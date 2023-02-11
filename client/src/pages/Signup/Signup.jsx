828388a1a2044615ba79467e4a7ecf8929c68ee3
n00bNn00b <dewanreal@gmail.com>
2023-02-11 21:55:43 +0600

    sign up logo added

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl my-20 flex mx-auto">
-        <figure className="px-10 pt-10">
+    <div className="my-20 flex mx-auto w-full">
+      <img
+        src="https://i.ibb.co/PNmzYRX/diginventory.png"
+        className="lg:flex md:hidden sm:hidden"
+        alt="logo"
+      />
+      <div className="card w-96 bg-base-100 shadow-xl flex mx-auto">
+        <figure className="px-5">
           <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
+            src="https://i.ibb.co/PNmzYRX/diginventory.png"
+            alt="logo"
             className="rounded-xl"
           />
         </figure>
@@ -38,12 +43,19 @@ const Signup = () => {
             placeholder="Create a password"
             className="input input-bordered w-full max-w-xs"
           />
-          <input type="button" className="btn btn-primary" value="Sign Up" />
           <input
             type="button"
-            className="btn bg-base-100 text-neutral hover:bg-yellow-100"
-            value="Sign Up with Google"
+            className="btn btn-primary hover:text-base-100"
+            value="Sign Up"
           />
+          <button className="btn bg-base-100 text-neutral hover:bg-yellow-100">
+            <img
+              className="h-10 w-10"
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+              alt="google"
+            />
+            Sign up with Google
+          </button>
         </div>
         <p className="text-center mb-5">
           Already have an account?{" "}
